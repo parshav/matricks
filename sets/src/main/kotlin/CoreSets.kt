187de93711a9014c035e4dd534b77821a661cee4
Parshav Chauhan <pchauhan@clientresourcesinc.com>
2019-01-27 11:26:17 -0600

    Added array union, intersect and disjoints

--- sets/src/main/kotlin/CoreSets.kt
+++ sets/src/main/kotlin/CoreSets.kt
@@ -4,4 +4,29 @@ class Test {
 
     fun test() {
     }
+
 }
+
+
+/**
+ * Elements present in either a or b arrays,
+ * */
+infix fun <T> Array<T>.union(to: Array<T>): Set<T> {
+    val returnList = mutableSetOf<T>()
+    returnList.addAll(this)
+    returnList.addAll(to)
+    return returnList
+}
+
+
+/**
+ * Elements in both arrays
+ * */
+infix fun <T> Array<T>.intersect(to: Array<T>): Set<T> = to.filter { it == this }.toSet()
+
+/**
+ * Is disjoint if no matching elements b/w arrays
+ * */
+fun <T> isDisjoint(firstArray: Array<T>, secondArray: Array<T>): Boolean = firstArray.intersect(secondArray).isEmpty()
+
+infix fun <T> Array<T>.isDisjointWith(to: Array<T>) = isDisjoint(this, to)
